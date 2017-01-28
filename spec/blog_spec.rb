e466138a0b89dda509a4c2b67d75c6d48de76752
C80609A <c080609a@gmail.com>
2017-01-28 10:28:53 +0300

    master: 2.5 Contexts: сгруппировал сценарии по контекстам, используя context и let.

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -1,17 +1,36 @@
 require 'blog'
 require 'post'
 
+# Изучаем контексты по видео '2.5 Contexts.mp4' (см. untitled: career: learning ruby в за это число +- 2дня, см. коммиты)
+# 1. Сначала были написаны 3 сценария.
+# 2. Затем было обращено внимание на общие вещи.
+# 3. После чего сценарии были сгруппированы с помощью инструкции context (вспомогательно использовалась let).
+
 RSpec.describe Blog do
 
-  it 'has no posts' do
-    blog = Blog.new
-    expect(blog).to be_empty
+  context 'with no posts' do
+
+    let(:blog) { Blog.new }
+
+    it 'has no posts' do
+      blog = Blog.new
+      expect(blog).to be_empty
+    end
+
   end
 
-  it 'has a post when initialized with one' do
-    blog = Blog.new([Post.new])
-    expect(blog.posts_count).to eq 1
-  end
+  context 'with one post' do
 
+    let(:blog) { Blog.new([Post.new]) }
+
+    it 'has a post when initialized with one' do
+      expect(blog).not_to be_empty
+    end
+
+    it 'count the number of posts' do
+      expect(blog.posts_count).to eq 1
+    end
+
+  end
 
 end
