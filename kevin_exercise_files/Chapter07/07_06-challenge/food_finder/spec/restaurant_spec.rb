33d34553245050d6d2703d4e92ee174f00f21328
C80609A <c080609a@gmail.com>
2017-05-01 14:06:42 +0300

    043 challenge solved: моё решение

--- kevin_exercise_files/Chapter07/07_06-challenge/food_finder/spec/restaurant_spec.rb
+++ kevin_exercise_files/Chapter07/07_06-challenge/food_finder/spec/restaurant_spec.rb
@@ -6,16 +6,39 @@ describe Restaurant do
   let(:crescent) { Restaurant.new(:name => 'Crescent', :cuisine => 'paleo', :price => '321') }
   
   describe 'attributes' do
-  
-    it 'allow reading and writing for :name'
 
-    it 'allow reading and writing for :cuisine'
+    # мои решения в этом блоке построены по следующему принципу:
+    # раз уж требуется в одном тесте проверить и reading и writing,
+    # то сначала мы читаем оригинальное значение (сохраняя его в переменную `read_*`),
+    # а затем пишем новое значение (сохраняя его в переменную `write_*`).
+    # После - лабаем массив из этих двух переменных и проверяем его
 
-    it 'allow reading and writing for :price'
+    it 'allow reading and writing for :name' do
+      read_name = crescent.name
+      crescent.name = 'Caesar'
+      write_name = crescent.name
+      expect([read_name, write_name]).to eq %w(Crescent Caesar)
+    end
+
+    it 'allow reading and writing for :cuisine' do
+      read_cuisine = crescent.cuisine
+      crescent.cuisine = 'mexican'
+      write_cuisine = crescent.cuisine
+      expect([read_cuisine, write_cuisine]).to eq %w(paleo mexican)
+    end
+
+    it 'allow reading and writing for :price' do
+      read_price = crescent.price
+      crescent.price = '123'
+      write_price = crescent.price
+      expect([read_price, write_price]).to eq %w(321 123)
+    end
     
   end
   
   describe '.load_file' do
+
+    # сказано - пропустить, уже сделано
 
     it 'does not set @@file if filepath is nil' do
       no_output { Restaurant.load_file(nil) }
@@ -46,21 +69,26 @@ describe Restaurant do
   end
   
   describe '.all' do
-    
+
     it 'returns array of restaurant objects from @@file' do
-      pending
+
+      # В этом примере необходимо проверить,
+      # что метод класса просто возвращает массив из 6 объектов
+      # типа Restaurant.
+      # Пример тривиальный, решение лёгкое, можно было бы
+      # ограничится одной строкой для затравки.
+
       Restaurant.load_file(test_file)
       restaurants = Restaurant.all
-      expect(restaurants.class).to eq('FIXME')
-      expect(restaurants.length).to eq('FIXME')
-      expect(restaurants.first.class).to eq('FIXME')
+      expect(restaurants.class).to eq(Array)
+      expect(restaurants.length).to eq(6)
+      expect(restaurants.first.class).to eq(Restaurant)
     end
 
     it 'returns an empty array when @@file is nil' do
-      pending
       no_output { Restaurant.load_file(nil) }
       restaurants = Restaurant.all
-      expect(restaurants).to eq('FIXME')
+      expect(restaurants).to eq([])
     end
     
   end
@@ -71,20 +99,35 @@ describe Restaurant do
       # subject would return the same thing
       let(:no_options) { Restaurant.new }
 
-      it 'sets a default of "" for :name'
+      it 'sets a default of "" for :name' do
+        no_options.name = ''
+      end
 
-      it 'sets a default of "unknown" for :cuisine'
+      it 'sets a default of "unknown" for :cuisine' do
+        no_options.cuisine = 'unknown'
+      end
 
-      it 'does not set a default for :price'
+      it 'does not set a default for :price' do
+        no_options.price = nil
+      end
     end
     
     context 'with custom options' do
-      
-      it 'allows setting the :name'
 
-      it 'allows setting the :cuisine'
+      subject(:with_options) { Restaurant.new({:name => '1', :cuisine => '2', :price => '3'}) }
 
-      it 'allows setting the :price'
+      it 'allows setting the :name' do
+        expect(with_options.name).to eq '1'
+      end
+
+      it 'allows setting the :cuisine' do
+        expect(with_options.cuisine).to eq '2'
+      end
+
+      it 'allows setting the :price' do
+        expect(with_options.price).to eq '3'
+
+      end
 
     end
 
@@ -92,31 +135,71 @@ describe Restaurant do
   
   describe '#save' do
     
-    it 'returns false if @@file is nil'
+    it 'returns false if @@file is nil' do
+      expect(crescent.save).to eq false
+    end
     
-    it 'returns false if not valid'
+    it 'returns false if not valid' do
+      crescent.price = -1
+      expect(crescent.save).to eq false
+    end
     
-    it 'calls append on @@file if valid'
+    it 'calls append on @@file if valid' do
+
+      # Здесь используется 'Partial Test Double',
+      # чтобы не записывать изменения в файл,
+      # не портить его, так скажем
+
+      Restaurant.load_file(test_file)
+      allow(Restaurant.file).to receive(:append).and_return(true)
+      expect(crescent.save).to eq true
+    end
     
   end
   
   describe '#valid?' do
+
+    # я решил опять применить Partial Test Double
+    # так же можно?
+
+    it 'returns false if name is nil' do
+      allow(crescent).to receive(:name).and_return(nil)
+      expect(crescent.valid?).to eq false
+    end
+
+    it 'returns false if name is blank' do
+      allow(crescent).to receive(:name).and_return('')
+      expect(crescent.valid?).to eq false
+    end
+
+    it 'returns false if cuisine is nil' do
+      allow(crescent).to receive(:cuisine).and_return(nil)
+      expect(crescent.valid?).to eq false
+    end
+
+    it 'returns false if cuisine is blank' do
+      allow(crescent).to receive(:cuisine).and_return('')
+      expect(crescent.valid?).to eq false
+    end
     
-    it 'returns false if name is nil'
+    it 'returns false if price is nil' do
+      allow(crescent).to receive(:price).and_return(nil)
+      expect(crescent.valid?).to eq false
+    end
 
-    it 'returns false if name is blank'
-
-    it 'returns false if cuisine is nil'
-
-    it 'returns false if cuisine is blank'
+    it 'returns false if price is 0' do
+      allow(crescent).to receive(:price).and_return(0)
+      expect(crescent.valid?).to eq false
+    end
     
-    it 'returns false if price is nil'
+    it 'returns false if price is negative' do
+      allow(crescent).to receive(:price).and_return(-1)
+      expect(crescent.valid?).to eq false
+    end
 
-    it 'returns false if price is 0'
-    
-    it 'returns false if price is negative'
-
-    it 'returns true if name, cuisine, price are present'
+    it 'returns true if name, cuisine, price are present' do
+      expect(crescent.valid?).to eq true
+    end
     
   end
 
